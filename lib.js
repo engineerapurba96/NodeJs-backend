a17d54f34388b57d21a430ff9a61a9626e37799b
engineerapurba96 <apurbaranjan.tech@gmail.com>
2024-01-06 17:16:16 +0530

    Initial commit

---
+++ lib.js
@@ -0,0 +1,18 @@
+//ES6
+// const sum =(a,b)=>{
+//     return a+b;
+// }
+
+// const diff =(a,b)=>{
+//     return a-b;
+// }
+// export {sum,diff}
+//End 
+exports.sum =(a,b)=>{
+    return a+b;
+}
+
+exports.diff =(a,b)=>{
+    return a-b;
+}
+
